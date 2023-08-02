5803ee31f01a3dfaac47efa9c031a547bc873581
Iamneo <techadmin@iamneo.ai>
2023-08-02 17:06:29 +0000

    Backup Commit

--- jspservlet-app-bmi-calculator/src/main/java/controller/CalculateBMI.java
+++ jspservlet-app-bmi-calculator/src/main/java/controller/CalculateBMI.java
@@ -7,9 +7,20 @@ public class CalculateBMI {
 		return (height/weight);
 	}
 	public static String description(double value){
-		if(value <= 18.5){
+		if(value < 18.5){
 			return "Under Weight";
 		}
-		else if(value)
+		else if(value >= 18.5 && value < 24.9){
+			return "Normal";
+		}
+		else if(value >= 25 && value < 29.9){
+			return "Over Weight";
+		}
+		else if(value >= 30 && value < 34.9){
+			return "Obese";
+		}
+		else{
+			return "Extremely Obese";
+		}
 	}
 }
